47008fa26ab5b36df882ed336e17653f0a9dbf0f
TXP2023 <bobi20110114@outlook.com>
2024-11-11 17:22:32 +0800

    run

--- run.cpp
+++ run.cpp
@@ -1,10 +1,12 @@
 #include <Windows.h>
+#include <iostream>
 
 int main() {
     system("g++ data.cpp -o data.exe");
     system("g++ Correct_code.cpp -o Correct_code.exe");
     system("g++ my_code.cpp -o my_code.exe");
+    std::cout << "finish comlpile" << std::endl;
 
-    system("strart \"check.bat\"");
+    system("strart check.bat");
     return 0;
 }
